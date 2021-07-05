73b0e052f8229f16b7ae5c7c5abc6ec57aa374f1
Andrew <74262186+cevaboyz@users.noreply.github.com>
2021-07-05 12:20:30 +0200

    Update matcher_hotjar2.R

--- matcher_hotjar2.R
+++ matcher_hotjar2.R
@@ -1,25 +1,4 @@
-#install and load package arules
-#install.packages("arules")
-library(arules)
-#install and load arulesViz
-#install.packages("arulesViz")
-library(arulesViz)
-#install and load tidyverse
-#install.packages("tidyverse")
-library(tidyverse)
-#install and load readxml
-#install.packages("readxml")
-library(readxl)
-#install and load knitr
-#install.packages("knitr")
-library(knitr)
-#load ggplot2 as it comes in tidyverse
-library(ggplot2)
-#install and load lubridate
-#install.packages("lubridate")
-library(lubridate)
-#install and load plyr
-#install.packages("plyr")
+
 library(plyr)
 library(dplyr)
 library(openxlsx)
@@ -38,13 +17,15 @@ hotjar <- read_excel("hotjar.xlsx")
 
 ##########################################################
 
-names(analytics)[2]<-"id"
+
 
 names(hotjar)[18]<- "id"
 
 hotjar<- subset(hotjar, select= 18)
 
 analytics <- analytics %>% select(id = "Hotjar User Id")
+
+names(analytics)[2]<-"id"
 
 #analytics <- subset(analytics, select= 2)
 
